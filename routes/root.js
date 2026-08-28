5fa32f595164ea24a63f126559b8bd3fd177ac9c
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-08-28 17:25:15 -0500

    feat: add /about and /api/users routes

--- routes/root.js
+++ routes/root.js
@@ -2,4 +2,12 @@ export default async (app, opts) => {
   app.get('/', (req, res) => {
     res.send("Hello World!");
   });
+
+  app.get("/about", (req, res) => {
+    res.send("About this project");
+  });
+
+  app.get("/api/users", (req, res) => {
+    res.json([{ id: 1, name: "Felipe Montoya" }])
+  })
 };
