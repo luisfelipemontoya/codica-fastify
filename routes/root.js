3a761d49580adde0d2f0f3f4d2ab6247db5bda36
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-08-28 21:02:28 -0500

    feat: add /users GET and POST routes for lesson

--- routes/root.js
+++ routes/root.js
@@ -7,6 +7,14 @@ export default async (app, opts) => {
     res.send("About this project");
   });
 
+  app.get("/users", (req, res) => {
+    res.send("GET /users");
+  });
+
+  app.post("/users", (req, res) => {
+    res.send("POST /users");
+  });
+
   app.get("/api/users", (req, res) => {
     res.json([{ id: 1, name: "Felipe Montoya" }])
   })
