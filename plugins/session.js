16a4f57fef632b7eee0b6587fbcbff6f1e39011f
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-09-09 16:57:34 -0500

    feat: use environment variables for session config

--- plugins/session.js
+++ plugins/session.js
@@ -3,9 +3,9 @@ import session from "@fastify/session";
 
 export default fp(async (app, options) => {
   await app.register(session, {
-    secret: 'una-clave-secreta-de-32-caracteres-minimo', // 32 caracteres mínimo
+    secret: process.env.SESSION_SECRET || 'una-clave-secreta-de-32-caracteres-minimo', // 32 caracteres mínimo
     cookie: {
-      secure: false, // true en producción con HTTPS
+      secure: process.env.NODE_ENV === 'production', // true en producción, false en desarrollo
       maxAge: 24 * 60 * 60 * 1000, // 24 horas en milisegundos       
       httpOnly: true, 
       sameSite: 'lax', //Controla si las cookies se envían en solicitudes entre sitios (protección CSRF)
